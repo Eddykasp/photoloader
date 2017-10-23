c49672e2b3c35948e4bc8d2c4f1b6ae3aa2c3a95
Eddykasp <maxkasperowski@gmail.com>
2017-10-23 21:10:38 +0100

    add upload exporter stub

---
+++ photoloader/src/main/java/com/maxkasp/photoloader/UploadSeriesExporter.java
@@ -0,0 +1,13 @@
+package com.maxkasp.photoloader;
+
+import java.io.IOException;
+
+public class UploadSeriesExporter implements SeriesExporter {
+
+	@Override
+	public void export(Series series) throws IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
